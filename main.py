bfa6c33ddd3aa3260f796c88791d39011284e6d6
Guillaume <gducange@hotmail.com>
2021-08-26 15:46:22 +0200

    Ajout main.py et ui/Application.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from ui.Application import Application
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    app = Application()
+    app.title("Ma Premiere App :-)")
+    app.mainloop()
+
